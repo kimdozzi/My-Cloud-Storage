70ed09fb1fa48c8bf770350eba25ca212595609d
kimdozzi <neo5188@gmail.com>
2024-05-25 00:19:11 +0900

    self-hosted runner main.yml 설정 및 testContoller 생성

---
+++ src/main/java/com/example/deploy/DeployController.java
@@ -0,0 +1,15 @@
+package com.example.deploy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DeployController {
+    @GetMapping("/")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("배포 자동화 테스트");
+    }
+
+}
