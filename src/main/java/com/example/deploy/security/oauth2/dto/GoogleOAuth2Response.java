3e742a9d1731ff6db3ae11bf151d4458e443ca70
kimdozzi <neo5188@gmail.com>
2024-08-12 09:50:37 +0900

    OAuth2 DTO
    - CustomOAuth2User
    - GoogleOAuth2Response
    - NaverOAuth2Response
    - OAuth2Response
    - UserDTO

--- src/main/java/com/example/deploy/security/oauth2/dto/GoogleOAuth2Response.java
+++ src/main/java/com/example/deploy/security/oauth2/dto/GoogleOAuth2Response.java
@@ -1,4 +1,36 @@
 package com.example.deploy.security.oauth2.dto;
 
-public class GoogleOAuth2Response {
+import java.util.Map;
+
+public class GoogleOAuth2Response implements OAuth2Response{
+
+    private final Map<String, Object> attribute;
+
+    public GoogleOAuth2Response(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProvider() {
+
+        return "google";
+    }
+
+    @Override
+    public String getProviderId() {
+
+        return attribute.get("sub").toString();
+    }
+
+    @Override
+    public String getEmail() {
+
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+
+        return attribute.get("name").toString();
+    }
 }
--- src/main/java/com/example/deploy/security/oauth2/dto/UserDTO.java
+++ src/main/java/com/example/deploy/security/oauth2/dto/UserDTO.java
@@ -1,2 +1,20 @@
-package com.example.deploy.security.oauth2.dto;public class UserDTO {
+package com.example.deploy.security.oauth2.dto;
+
+import lombok.Builder;
+
+@Builder
+public record UserDTO (
+        String role,
+        String name,
+        String username,
+        String email
+){
+    public static UserDTO of(String role, String name, String username, String email) {
+        return UserDTO.builder()
+                .role(role)
+                .name(name)
+                .username(username)
+                .email(email)
+                .build();
+    }
 }
